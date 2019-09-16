fae36e8a3da10daa2e1c72d1fdd4324b94e4f01b
Matthias Treydte <mt@waldheinz.de>
2019-09-16 10:12:00 +0200

    .config/nixpkgs: provide a vscode with nix and shellcheck extensions

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -24,10 +24,29 @@ with import <nixpkgs> {};
             };
         };
 
+        myVscode = vscode-with-extensions.override {
+            vscodeExtensions = vscode-utils.extensionsFromVscodeMarketplace [
+                {
+                    name = "shellcheck";
+                    publisher = "timonwong";
+                    version = "0.8.1";
+                    sha256 = "0zg7ihwkxg0da0wvqcy9vqp6pyjignilsg9cldp5pp9s0in561cw";
+                }
+
+                {
+                    name = "Nix";
+                    publisher = "bbenoist";
+                    version = "1.0.1";
+                    sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
+                }
+            ];
+        };
+
         all = with pkgs; buildEnv {
             name = "all";
             paths = [
                 myNeovim
+                myVscode
                 nodePackages.typescript
             ];
         };
